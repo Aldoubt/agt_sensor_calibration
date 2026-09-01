17e3fe94a5da42e55c536900698202cfa5b60b8c
Xuan Yang <xuanyang.robotics@gmail.com>
2026-09-01 12:45:29 +0800

    Add optional extrinsic prior optimization interface

--- src/agt_extrinsic_calibration/include/agt_extrinsic_calibration/optimizer.hpp
+++ src/agt_extrinsic_calibration/include/agt_extrinsic_calibration/optimizer.hpp
@@ -5,6 +5,13 @@
 
 namespace agt_extrinsic_calibration {
 
+struct ExtrinsicPrior {
+  bool enabled{false};
+  Eigen::Isometry3d transform{Eigen::Isometry3d::Identity()};
+  double translation_sigma{0.05};
+  double rotation_sigma_rad{0.0872664626};
+};
+
 struct OptimizationReport {
   Eigen::Isometry3d transform{Eigen::Isometry3d::Identity()};
   double initial_rmse{0.0};
@@ -16,6 +23,8 @@ struct OptimizationReport {
 };
 
 OptimizationReport optimize_extrinsic(const std::vector<PosePair> &pairs);
+OptimizationReport optimize_extrinsic(const std::vector<PosePair> &pairs,
+                                      const ExtrinsicPrior &prior);
 void write_result(const OptimizationReport &report, const std::string &yaml_file,
                   const std::string &markdown_file);
 
